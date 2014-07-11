196748af5a6b4e4f17d369335ba1d4a64d04f427
Giuseppe Amatulli of group sbsc <ga254@login-0-0.local>
2014-07-11 14:29:20 -0400

    complite AOD cicle integration

--- AE_C6_MYD04_L2/sc4_animationR.sh
+++ AE_C6_MYD04_L2/sc4_animationR.sh
@@ -1,20 +1,121 @@
+# module load Apps/R/3.0.2 
+# source ("/home/fas/sbsc/ga254/scripts/AE_C6_MYD04_L2/sc4_animationR.sh")
+# .libPaths( c( .libPaths(), "/home/fas/sbsc/ga254/R/x86_64-unknown-linux-gnu-library/3.0") )
+
+library(raster)
+library(lattice , lib.loc ='/home/fas/sbsc/ga254/R/x86_64-unknown-linux-gnu-library/3.0')
+library(rasterVis)
+library(foreach)
+
+ #  gdal_translate  -co COMPRESS=LZW -co ZLEVEL=9  -ot Int32  -a_nodata -9999   integration001.tif    integration001a.tif
+
+rmr=function(x){
+## function to truly delete raster and temporary files associated with them
+if(class(x)=="RasterLayer"&grepl("^/tmp",x@file@name)&fromDisk(x)==T){
+file.remove(x@file@name,sub("grd","gri",x@file@name))
+rm(x)
+}
+}
+
+
+path = "/lustre/scratch/client/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/temp_smoth_10km/"
+
+# for parallel
+# you can use a foreach to do it in parallel too, if you like
+
+
+# foreach(file=files.list) %dopar%{
+
+for(day  in c(1:365) ) {
+ 
+basename=paste("AOD_10km_day",day,sep="")
+## not sure exactly what this does, but there is basename(file) too...
+
+png(paste("/lustre/scratch/client/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/png_smoth_10km/",basename,".png",sep=""),width=2000,height=1000)
+
+## add width and height to specify the size of the png, otherwise it will be small (100x100, I think)
+ 
+day001=raster(paste("/lustre/scratch/client/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/temp_smoth_10km/AOD_10km_day",day,".tif",sep=""))
+
+refl001=1-(2.718281828 ^ ( 0 - (day001/1000  ) ))
+## be careful with this, it will recalculate the whole image into a temporary directory and not automatically delete it..
+## I would suggest deleting it at the end of the loop...
+ 
+n=100
+at=seq(0,1,length=n)
+colR=colorRampPalette(c("yellow", "red", "brown"))
+ 
+cols=colR(n)
+res=1e6 # res=1e4 for testing and res=1e6 for the final product
+greg=list(ylim=c(-90,90),xlim=c(-180,180))
+ 
+print (  levelplot(refl001,col.regions=colR(n) , cuts=99,at=at,colorkey=list(space="right",adj=1), panel=panel.levelplot.raster,margin=F,maxpixels=res,ylab="",xlab="",useRaster=T,ylim=greg$ylim) + layer(panel.text(160, 85, paste('Julian day ',day,sep=""),cex=2 ))  )
+
+rmr(refl001) # really  remove raster files, this will delete the temporary file
+ 
+dev.off() 
+ 
+}
+
+# move the data to litoria /home/giuseppea/tmp/animation_MERRAero  and run ffmepg 
+#  ffmpeg -r 25  -i mean%03d.png    -vcodec libx264  -pix_fmt yuv420p -r 25 animation_MERRAero.mp4
 
 
 
-pkcreatect   -min 1 -max 255  >  color.txt
-cat /lustre0/scratch/ga254/dem_bj/AE_C6_MYD04_L2/day_list.txt | xargs -n 1 -P 10 bash -c  $'
-DAYc=$1
-gdal_translate -scale  0 5000 1 255  -ot Byte -a_nodata 0   AOD_550_mean_day${DAYc}.tif  AOD_550_mean_day${DAYc}_byte.tif
+# ls integration???.tif | xargs -n 1 -P 20 bash -c $' 
 
-pkcreatect  -ct   color.txt -co COMPRESS=LZW -co ZLEVEL=9  -i  AOD_550_mean_day${DAYc}_byte.tif -o   AOD_550_mean_day${DAYc}_byte_ct.tif
+# export file=$1
 
-' _ 
+# R  --vanilla --no-readline   -q  <<EOF
+
+# library(raster)
+# library(lattice , lib.loc ="/home2/ga254/R/x86_64-unknown-linux-gnu-library/3.0")
+# library(rasterVis)
+# library(foreach)
+
+# rmr=function(x){
+# ## function to truly delete raster and temporary files associated with them
+# if(class(x)=="RasterLayer"&grepl("^/tmp",x@file@name)&fromDisk(x)==T){
+# file.remove(x@file@name,sub("grd","gri",x@file@name))
+# rm(x)
+# }
+# }
+
+# file = Sys.getenv(c("file"))
+# print(file)
+
+# basename=substr(file,0,14)
+# print(basename)
+
+# day=substr (basename,12,14)
+ 
+# png(paste("/lustre0/scratch/ga254/dem_bj/AE_C51_C6_L2/integration_15/png/",basename , ".png" ,sep=""),width=2000,height=1000)
+# ## add width and height to specify the size of the png, otherwise it will be small (100x100, I think)
+ 
+# day001=raster(paste("/lustre0/scratch/ga254/dem_bj/AE_C51_C6_L2/integration_15/tif/",file,sep=""))
+ 
+# refl001=1-(2.718281828 ^ ( 0 - (day001 * 0.001 ) ))
+# ## be careful with this, it will recalculate the whole image into a temporary directory and not automatically delete it..
+# ## I would suggest deleting it at the end of the loop...
+ 
+# n=100
+# at=seq(0,1,length=n)
+# colR=colorRampPalette(c("yellow", "red", "brown"))
+ 
+# cols=colR(n)
+# res=1e6 # res=1e4 for testing and res=1e6 for the final product
+# greg=list(ylim=c(-90,90),xlim=c(-180,180))
+ 
+# print (  levelplot(refl001,col.regions=colR(n) , cuts=99,at=at,colorkey=list(space="right",adj=1), panel=panel.levelplot.raster,margin=F,maxpixels=res,ylab="",xlab="",useRaster=T,ylim=greg\$ylim) + layer(panel.text(160, 85, paste("Julian day ",day,sep=""),cex=3 ))  )
+
+# rmr(refl001) # really  remove raster files, this will delete the temporary file
+ 
+# dev.off()
+
+# EOF
+
+# ' _
 
 
-cat /lustre0/scratch/ga254/dem_bj/AE_C6_MYD04_L2/day_list.txt xargs -n 1 echo AOD_550_mean_day${$1}_byte_ct.tif 
-
-
-
-convert -scale 50%  -delay 200 -loop 0 $(head -40 /lustre0/scratch/ga254/dem_bj/AE_C6_MYD04_L2/day_list.txt | xargs -n 1 bash -c $' echo 'AOD_550_mean_day'$1'_byte_ct.tif'  ' _ ) test.gif 
 
 
--- AE_C6_MYD04_L2/sc6_warp1km.sh
+++ AE_C6_MYD04_L2/sc6_warp1km.sh
@@ -4,7 +4,7 @@
 
 #PBS -S /bin/bash
 #PBS -q fas_normal
-#PBS -l walltime=01:00:00
+#PBS -l walltime=1:30:00  # 1 hour for the full cicle 
 #PBS -l nodes=1:ppn=1
 #PBS -V
 #PBS -o /scratch/fas/sbsc/ga254/stdout
@@ -13,18 +13,27 @@
 
 export INDIR=/scratch/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/temp_smoth
 export OUTDIR=/scratch/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/temp_smoth_1km
+export OUTDIR_10km=/scratch/fas/sbsc/ga254/dataproces/AE_C6_MYD04_L2/temp_smoth_10km
 
 # export DAY=$1
 
 DAY=$DAY
 
-rm -f  $OUTDIR/merge$DAY.vrt  $OUTDIR/merge$DAY.tif 
+rm -f  $OUTDIR/merge$DAY.vrt 
 gdalbuildvrt  -overwrite  $OUTDIR/merge$DAY.vrt    $INDIR/smoth_mean_alldays??_band$DAY.tif
 
+gdal_translate -ot  Float32    -co  COMPRESS=LZW -co ZLEVEL=9    $OUTDIR/merge$DAY.vrt    $OUTDIR_10km/merge$DAY.tif 
+
+pkgetmask -i   $OUTDIR_10km/merge$DAY.tif   -min -100 -max 1   -data 0  -nodata 1  -co  COMPRESS=LZW -co ZLEVEL=9    -o   $OUTDIR_10km/merge$DAY"_msk".tif 
+pksetmask -i   $OUTDIR_10km/merge$DAY.tif    -m    $OUTDIR_10km/merge$DAY"_msk".tif  -co  COMPRESS=LZW -co ZLEVEL=9    -msknodata 0 -nodata 0   -o   $OUTDIR_10km/AOD_10km_day$DAY.tif 
+gdal_edit.py   -a_nodata -1   $OUTDIR_10km/AOD_10km_day$DAY.tif 
+rm   $OUTDIR_10km/merge$DAY"_msk".tif    $OUTDIR/merge$DAY.vrt    $OUTDIR_10km/merge$DAY.tif 
+
 echo warping   $OUTDIR/merge$DAY.tif 
-gdalwarp  -ot   Float32  -co BIGTIFF=YES  -co  COMPRESS=LZW -co ZLEVEL=9   -r cubicspline -tr 0.008333333333333333  0.008333333333333333  $OUTDIR/merge$DAY.vrt    $OUTDIR/merge$DAY.tif 
-gdal_translate -ot Int16    -co BIGTIFF=YES  -co  COMPRESS=LZW -co ZLEVEL=9   $OUTDIR/merge$DAY.tif  $OUTDIR/AOD_day$DAY.tif
-rm -f  $OUTDIR/merge$DAY.tif  
+gdalwarp  -srcnodata -1 -dstnodata -1  -ot   Float32  -co BIGTIFF=YES  -co  COMPRESS=LZW -co ZLEVEL=9   -r cubicspline -tr 0.008333333333333333  0.008333333333333333   $OUTDIR_10km/AOD_10km_day$DAY.tif   $OUTDIR/tmpAOD_1km_day$DAY.tif 
+gdal_translate -ot Int16  -a_nodata -1   -co  COMPRESS=LZW -co ZLEVEL=9     $OUTDIR/tmpAOD_1km_day$DAY.tif    $OUTDIR/AOD_1km_day$DAY.tif 
+
+rm -f  $OUTDIR/merge$DAY.tif   $OUTDIR/merge$DAY.vrt  $OUTDIR/tmpAOD_1km_day$DAY.tif 
 
 exit
 
