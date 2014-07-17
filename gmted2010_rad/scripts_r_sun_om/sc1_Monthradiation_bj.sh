5bca61b45414a0423e762ace1ab956cc4b1df93a
Giuseppe Amatulli of group sbsc <ga254@login-0-0.local>
2014-07-17 17:42:13 -0400

    tested horizonal with Alb AOD cloud

--- gmted2010_rad/scripts_r_sun_om/sc1_Monthradiation_bj.sh
+++ gmted2010_rad/scripts_r_sun_om/sc1_Monthradiation_bj.sh
@@ -12,16 +12,17 @@
 
 # for tile  in /lustre/scratch/client/fas/sbsc/ga254/dataproces/GMTED2010/tiles/be75_grd_tif/3_1.tif  ; do  bash   /home/fas/sbsc/ga254/scripts/gmted2010_rad/scripts_r_sun_om/sc1_Monthradiation_bj.sh  $tile ; done 
 
+# un tile ha impiegato 6 ore
 
 #PBS -S /bin/bash 
 #PBS -q fas_long
-#PBS -l walltime=72:00:00  
+#PBS -l walltime=8:00:00  
 #PBS -l nodes=1:ppn=8
 #PBS -V
-#PBS -o /lustre0/scratch/ga254/stdout 
-#PBS -e /lustre0/scratch/ga254/stderr
+#PBS -o  /scratch/fas/sbsc/ga254/stdout 
+#PBS -e  /scratch/fas/sbsc/ga254/stderr
 
-tile=$1
+# tile=$1
 
 file=`basename $tile`
 
@@ -41,9 +42,9 @@ cd $INDIRG
 
 # clip the 1km dem, use the be75_grd_tif to ensure larger overlap
 
-# gdal_translate -projwin  $(getCorners4Gtranslate  $INTIF/$file) $INTIF_mn30/mn30_grd.tif $file
+gdal_translate -projwin  $(getCorners4Gtranslate  $INTIF/$file) $INTIF_mn30/mn30_grd.tif $file
 
-gdal_translate -srcwin 9000 4200 50 50  $INTIF_mn30/mn30_grd.tif $file  # to create a file test 
+# gdal_translate -srcwin 9000 4200 50 50  $INTIF_mn30/mn30_grd.tif $file  # to create a file test 
 
 
 echo create location  loc_$filename 
@@ -93,11 +94,11 @@ r.slope.aspect elevation=$filename    aspect=aspect_$filename  slope=slope_$file
 
 echo calculate horizon
 
-step 1 in r.sun = horizonstep=15   360/24 = 15 
+# step 1 in r.sun = horizonstep=15   360/24 = 15 
 
 r.horizon  elevin=$filename   horizonstep=15  horizon=horiz   maxdistance=200000
 
-echo  001 017 033 049 065 080 097 113 129 145 161 177 193 209 225 241 257 273 289 305 321 337 353  | xargs -n 1  -P 1  bash  -c $'
+echo  001 017 033 049 065 081 097 113 129 145 161 177 193 209 225 241 257 273 289 305 321 337 353  | xargs -n 1  -P 8  bash  -c $'
 
 dayi=$1
 day=$(echo $dayi | awk \'{ print int($1) }\'  ) #   to have the integer value 
@@ -109,11 +110,12 @@ r.external  -o input=/lustre/scratch/client/fas/sbsc/ga254/dataproces/MODALB/0.3
 r.mapcalc  " albedo${day}_${filename}_coef =  albedo${day}_${filename}  * 0.001" 
 
 echo  import cloud
+# for this case import the same tif  for the full month
 
 monthc=$( awk -v day=$day \'{ if ($2==day) {print substr($1,0,2) } }\'  /lustre/scratch/client/fas/sbsc/ga254/dataproces/MERRAero/tif_day/MMDD_JD_0JD.txt  ) 
 
 r.external -o input=/lustre/scratch/client/fas/sbsc/ga254/dataproces/CLOUD/month/MCD09_mean_${monthc}.tif   output=cloud${day}_${filename}  --overwrite 
-r.mapcalc  " cloud${day}_${filename}_coef =  ( cloud${day}_${filename} * 0.001)" 
+r.mapcalc  " cloud${day}_${filename}_coef =  ( cloud${day}_${filename} * 0.0001)" 
 
 echo import Aerosol 
 
@@ -127,27 +129,75 @@ r.mapcalc " aeros${day}_${filename}_coef =  1 - (  2.718281828^(- (aeros${day}_$
 
 # horizontal clear sky 
 # take out aspin=aspect_$filename  slopein=slope_$filename to simulate horizontal behaviur    better specify the slope=0 
+# in case of horizontal behaviur the reflectance is = 0 
+# also the albedo dose not influence the  global radiation  
+# indead the glob_HradT_day${day}_month${monthc} = glob_HraAl_day${day}_month${monthc}
+
+# make anonther test with inclined surface
 
 # horizontal aerosl and cloud and albedo 
+
+# transparent = T
+
+r.sun  --o   elev_in=$filename  slope=0.01 \
+lin=1   \
+day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
+glob_rad=glob_HradT_day${day}_month${monthc} \
+diff_rad=diff_HradT_day${day}_month${monthc} \
+beam_rad=beam_HradT_day${day}_month${monthc} \
+refl_rad=refl_HradT_day${day}_month${monthc}     # orizontal 0 reflectance 
+
+# albedo - Al
+
+r.sun  --o   elev_in=$filename  slope=0.01 \
+lin=1   albedo=albedo${day}_${filename}_coef   \
+day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
+glob_rad=glob_HradAl_day${day}_month${monthc} \
+diff_rad=diff_HradAl_day${day}_month${monthc} \
+beam_rad=beam_HradAl_day${day}_month${monthc} \
+refl_rad=refl_HradAl_day${day}_month${monthc}     # orizontal 0 reflectance 
+
+# ALBEDO CLOUD AEROSOL
 
 r.sun  --o   elev_in=$filename  slope=0.01 \
 lin=1   albedo=albedo${day}_${filename}_coef   coef_bh=cloud${day}_${filename}_coef  coef_dh=aeros${day}_${filename}_coef \
 day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
-glob_rad=glob_HradCA_day$day \
-diff_rad=diff_HradCA_day$day \
-beam_rad=beam_HradCA_day$day \
-refl_rad=refl_HradCA_day$day     # orizontal 0 reflectance 
+glob_rad=glob_HradACA_day${day}_month${monthc} \
+diff_rad=diff_HradACA_day${day}_month${monthc} \
+beam_rad=beam_HradACA_day${day}_month${monthc} \
+refl_rad=refl_HradACA_day${day}_month${monthc}     # orizontal 0 reflectance 
 
+# CLOUD AOD 
 
-# r.sun --o  elev_in=$filename  slope=0.01 \
-# lin=1   albedo=albedo${day}_${filename}_coef    \
-# day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
-# glob_rad=glob_Hrad_day$day \
-# diff_rad=diff_Hrad_day$day \
-# beam_rad=beam_Hrad_day$day \
-# refl_rad=refl_Hrad_day$day # orizontal 0 reflectance 
+r.sun  --o   elev_in=$filename  slope=0.01 \
+lin=1     coef_bh=cloud${day}_${filename}_coef  coef_dh=aeros${day}_${filename}_coef \
+day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
+glob_rad=glob_HradCA_day${day}_month${monthc} \
+diff_rad=diff_HradCA_day${day}_month${monthc} \
+beam_rad=beam_HradCA_day${day}_month${monthc} \
+refl_rad=refl_HradCA_day${day}_month${monthc}     # orizontal 0 reflectance 
 
-# g.remove rast=linke${day}_${filename}_coef,albedo${day}_${filename}_coef,cloud${day}_${filename}_coef,aeros${day}_${filename}_coef
+# CLOUD 
+
+r.sun  --o   elev_in=$filename  slope=0.01 \
+lin=1   coef_bh=cloud${day}_${filename}_coef \
+day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
+glob_rad=glob_HradC_day${day}_month${monthc} \
+diff_rad=diff_HradC_day${day}_month${monthc} \
+beam_rad=beam_HradC_day${day}_month${monthc} \
+refl_rad=refl_HradC_day${day}_month${monthc}     # orizontal 0 reflectance 
+
+# AOD
+
+r.sun  --o   elev_in=$filename  slope=0.01 \
+lin=1  coef_dh=aeros${day}_${filename}_coef \
+day=$day step=1 horizon=horiz  horizonstep=15   --overwrite  \
+glob_rad=glob_HradA_day${day}_month${monthc} \
+diff_rad=diff_HradA_day${day}_month${monthc} \
+beam_rad=beam_HradA_day${day}_month${monthc} \
+refl_rad=refl_HradA_day${day}_month${monthc}     # orizontal 0 reflectance 
+
+g.mremove -f  rast=albedo${day}_${filename}_coef,cloud${day}_${filename}_coef,aeros${day}_${filename}_coef
 
 ' _
 
@@ -155,43 +205,33 @@ refl_rad=refl_HradCA_day$day     # orizontal 0 reflectance
 
 echo starting to calculate average at montly level
 
-echo 1 31 59 90 120 151 181 212 243 273 304 334 | xargs -n 1  -P 12  bash -c  $'  
+echo 01 02 03 04 05 06 07 08 09 10 11 12 | xargs  -n 1   -P 12  bash -c  $'  
 
-day=$1
+month=$1
 
-# nalb viene inserito per la computazione 2 (1) giorni al mese sulla base del valore di albedo 
+echo mean compupation aerosol and cloud and albedo  
 
-if [ $day -eq 1   ] ; then  month=1  ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
-if [ $day -eq 31  ] ; then  month=2  ; dayend=$(expr $day + 26 ) ; n=28 ; nalb=2; fi   # 28 days  
-if [ $day -eq 59  ] ; then  month=3  ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
-if [ $day -eq 90  ] ; then  month=4  ; dayend=$(expr $day + 28 ) ; n=30 ; nalb=2; fi   # 30 days  
-if [ $day -eq 120 ] ; then  month=5  ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
-if [ $day -eq 151 ] ; then  month=6  ; dayend=$(expr $day + 28 ) ; n=30 ; nalb=2; fi   # 30 days  
-if [ $day -eq 181 ] ; then  month=7  ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
-if [ $day -eq 212 ] ; then  month=8  ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
-if [ $day -eq 243 ] ; then  month=9  ; dayend=$(expr $day + 28 ) ; n=30 ; nalb=2; fi   # 30 days  
-if [ $day -eq 273 ] ; then  month=10 ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=1; fi   # 31 days  
-if [ $day -eq 304 ] ; then  month=11 ; dayend=$(expr $day + 28 ) ; n=30 ; nalb=2; fi   # 30 days  
-if [ $day -eq 334 ] ; then  month=12 ; dayend=$(expr $day + 29 ) ; n=31 ; nalb=2; fi   # 31 days  
+for INPUT in T Al ACA CA C A ; do 
 
-n=$nalb # da togliere in caso di run for the full year 
+r.series input=$(g.mlist rast pattern="glob_Hrad${INPUT}_day*_month${month}" sep=,)  output=tglob_Hrad${INPUT}_m$month   method=average  --overwrite 
+r.series input=$(g.mlist rast pattern="diff_Hrad${INPUT}_day*_month${month}" sep=,)   output=tdiff_Hrad${INPUT}_m$month   method=average  --overwrite 
+r.series input=$(g.mlist rast pattern="beam_Hrad${INPUT}_day*_month${month}" sep=,)   output=tbeam_Hrad${INPUT}_m$month   method=average  --overwrite 
+r.series input=$(g.mlist rast pattern="refl_Hrad${INPUT}_day*_month${month}" sep=,)   output=trefl_Hrad${INPUT}_m$month   method=average  --overwrite 
 
-echo mean compupation aerosol and cloud and albedo 
+r.mapcalc   "glob_Hrad${INPUT}_m$month  = int (  tglob_Hrad${INPUT}_m$month )"
+r.mapcalc   "diff_Hrad${INPUT}_m$month  = int (  tdiff_Hrad${INPUT}_m$month )"
+r.mapcalc   "beam_Hrad${INPUT}_m$month  = int (  tbeam_Hrad${INPUT}_m$month )"
+r.mapcalc   "refl_Hrad${INPUT}_m$month  = int (  trefl_Hrad${INPUT}_m$month )"
 
+g.remove rast=tglob_Hrad${INPUT}_m$month
+g.remove rast=tdiff_Hrad${INPUT}_m$month
+g.remove rast=tbeam_Hrad${INPUT}_m$month
+g.remove rast=trefl_Hrad${INPUT}_m$month
 
-
-
-
-
-r.mapcalc  " glob_HradCA_m$month = (  $(  echo $(for dayn in `seq $day $dayend` ;   do  echo  glob_HradCA_day${dayn} +  ; done)  glob_HradCA_day$(expr $day + $n - 1)  ) ) /$n " 
-r.mapcalc  " diff_HradCA_m$month = (  $(  echo $(for dayn in `seq $day $dayend` ;   do  echo  diff_HradCA_day${dayn} +  ; done)  diff_HradCA_day$(expr $day + $n - 1)  ) ) /$n "
-r.mapcalc  " beam_HradCA_m$month = (  $(  echo $(for dayn in `seq $day $dayend` ;   do  echo  beam_HradCA_day${dayn} +  ; done)  beam_HradCA_day$(expr $day + $n - 1)  ) ) /$n "
-r.mapcalc  " refl_HradCA_m$month = (  $(  echo $(for dayn in `seq $day $dayend` ;   do  echo  refl_HradCA_day${dayn} +  ; done)  refl_HradCA_day$(expr $day + $n - 1)  ) ) /$n "
-
-r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=glob_HradCA_m$month    output=$OUTDIR/glob_rad/glob_HradCA_month$month"_"$file
-r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=diff_HradCA_m$month    output=$OUTDIR/diff_rad/diff_HradCA_month$month"_"$file
-r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=beam_HradCA_m$month    output=$OUTDIR/beam_rad/beam_HradCA_month$month"_"$file
-r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=refl_HradCA_m$month    output=$OUTDIR/refl_rad/refl_HradCA_month$month"_"$file
+r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=glob_Hrad${INPUT}_m$month    output=$OUTDIR/glob_rad/glob_Hrad${INPUT}_month$month"_"$file
+r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=diff_Hrad${INPUT}_m$month    output=$OUTDIR/diff_rad/diff_Hrad${INPUT}_month$month"_"$file
+r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=beam_Hrad${INPUT}_m$month    output=$OUTDIR/beam_rad/beam_Hrad${INPUT}_month$month"_"$file
+r.out.gdal -c type=Float32  nodata=-1  createopt="COMPRESS=LZW,ZLEVEL=9"  input=refl_Hrad${INPUT}_m$month    output=$OUTDIR/refl_rad/refl_Hrad${INPUT}_month$month"_"$file
 
 
 if [ ${filename:0:1} -eq 0 ] ; then 
@@ -199,26 +239,34 @@ if [ ${filename:0:1} -eq 0 ] ; then
 
 # aerosol and cloud
 
-gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/glob_rad/glob_HradCA_month$month"_"$file  $OUTDIR/glob_rad/glob_HradCA_month$month"_"$file"_tmp"
-gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/diff_rad/diff_HradCA_month$month"_"$file  $OUTDIR/diff_rad/diff_HradCA_month$month"_"$file"_tmp"
-gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/beam_rad/beam_HradCA_month$month"_"$file  $OUTDIR/beam_rad/beam_HradCA_month$month"_"$file"_tmp"
-gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/refl_rad/refl_HradCA_month$month"_"$file  $OUTDIR/refl_rad/refl_HradCA_month$month"_"$file"_tmp"
+gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/glob_rad/glob_Hrad${INPUT}_month$month"_"$file  $OUTDIR/glob_rad/glob_Hrad${INPUT}_month$month"_"$file"_tmp"
+gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/diff_rad/diff_Hrad${INPUT}_month$month"_"$file  $OUTDIR/diff_rad/diff_Hrad${INPUT}_month$month"_"$file"_tmp"
+gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/beam_rad/beam_Hrad${INPUT}_month$month"_"$file  $OUTDIR/beam_rad/beam_Hrad${INPUT}_month$month"_"$file"_tmp"
+gdal_translate -co COMPRESS=LZW -co ZLEVEL=9 -a_ullr   $(getCorners4Gtranslate $INTIF/$file)  $OUTDIR/refl_rad/refl_Hrad${INPUT}_month$month"_"$file  $OUTDIR/refl_rad/refl_Hrad${INPUT}_month$month"_"$file"_tmp"
 
-mv $OUTDIR/glob_rad/glob_HradCA_month$month"_"$file"_tmp" $OUTDIR/glob_rad/glob_HradCA_month$month"_"$file
-mv $OUTDIR/diff_rad/diff_HradCA_month$month"_"$file"_tmp" $OUTDIR/diff_rad/diff_HradCA_month$month"_"$file
-mv $OUTDIR/beam_rad/beam_HradCA_month$month"_"$file"_tmp" $OUTDIR/beam_rad/beam_HradCA_month$month"_"$file
-mv $OUTDIR/refl_rad/refl_HradCA_month$month"_"$file"_tmp" $OUTDIR/refl_rad/refl_HradCA_month$month"_"$file
+mv $OUTDIR/glob_rad/glob_Hrad${INPUT}_month$month"_"$file"_tmp" $OUTDIR/glob_rad/glob_Hrad${INPUT}_month$month"_"$file
+mv $OUTDIR/diff_rad/diff_Hrad${INPUT}_month$month"_"$file"_tmp" $OUTDIR/diff_rad/diff_Hrad${INPUT}_month$month"_"$file
+mv $OUTDIR/beam_rad/beam_Hrad${INPUT}_month$month"_"$file"_tmp" $OUTDIR/beam_rad/beam_Hrad${INPUT}_month$month"_"$file
+mv $OUTDIR/refl_rad/refl_Hrad${INPUT}_month$month"_"$file"_tmp" $OUTDIR/refl_rad/refl_Hrad${INPUT}_month$month"_"$file
 
 fi
 
+g.mremove -f  "glob_Hrad${INPUT}_day*_month*"
+g.mremove -f  "diff_Hrad${INPUT}_day*_month*"
+g.mremove -f  "beam_Hrad${INPUT}_day*_month*"
+g.mremove -f  "refl_Hrad${INPUT}_day*_month*"
+
+
+done 
 
 ' _ 
+
 
 checkjob -v $PBS_JOBID
 
 exit 
 
-# for day in `seq 1 2` ; do r.info  glob_HradC_day11   ; done 
+
 
 
 
