85138a5d52816ea0e5346ad8ef8104b2b04a18b9
Giuseppe Amatulli of group sbsc <ga254@login-0-0.local>
2016-09-06 17:14:11 -0400

    aggregation after the mean and the regression

--- GEOING/sc2_temporal_slope_observation.sh
+++ GEOING/sc2_temporal_slope_observation.sh
@@ -27,76 +27,61 @@ SEQ=$(echo $YYYY  | tr "." " ")
 
 echo temperature CUR 
 
-# resampling from 0.5 to 1
+# year mean for temperature CRU 0.5 ; then regression
 
-cdo -P 2  remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ ) ; do echo -n ,$year ; done)  $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.tmp.dat.nc   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg_tmp.nc
+cdo yearmean  -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ ) ; do echo -n ,$year ; done) $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.tmp.dat.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc
 
-# year mean for 0.5 and 1 
-
-cdo yearmean    $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg_tmp.nc   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg.nc
-rm   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg_tmp.nc
-
-cdo yearmean  -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ ) ; do echo -n ,$year ; done)  $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.tmp.dat.nc   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc
+cdo regres  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.nc
 
 echo  precipitation CUR 
 
-# resampling to from 0.5 to 1 deg 
+# year sum for precipitation  CRU  ; then regression 
 
-cdo -P 2 remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt -selyear$(for year in $(seq $SEQ) ; do echo -n ,$year ; done)  $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.pre.dat.nc   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg_tmp.nc
-
-# year sum for precipitation 
-
-cdo yearsum    $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg_tmp.nc    $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg.nc
-rm $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg_tmp.nc
-
-cdo yearsum   -selyear$(for year in $(seq $SEQ); do echo -n ,$year ; done)   $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.pre.dat.nc   $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc
-
-echo temperature HadISST
-
-cdo yearmean  -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ); do echo -n ,$year; done) -select,param=-2 $DIR/HadISST/HadISST_sst.nc $DIR/HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc  
-
-echo  observation temporal yearly regression 
+cdo yearsum  -selyear$(for year in $(seq $SEQ); do echo -n ,$year ; done)   $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.pre.dat.nc    $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc
 
 cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.nc
-cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.nc
 
-cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.nc
-cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg.nc
+# year mean 
+cdo timmean  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.nc
+cdo timmean  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.nc
 
-cdo regres  $DIR/HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc     $DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.nc  
+
+# resampling the regression and the mean tmp and mean prep  # not implementet...implemented with pkfilter
+
+# cdo -P 2  remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.nc
+# cdo -P 2  remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg.nc
+# cdo -P 2  remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.nc
+# cdo -P 2  remapcon2,$DIR/HadISST/HadISST_sst_griddes.txt $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.nc
+
+echo temperature HadISST
+cdo yearmean  -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ); do echo -n ,$year; done) -select,param=-2 $DIR/HadISST/HadISST_sst.nc $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc  
+
+cdo timmean  $DIR/HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc     $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.nc  
+cdo regres  $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc      $DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.nc  
+
 
 # transform to tif 
-
 
 gdal_translate -ot Float32  -co COMPRESS=DEFLATE  -co ZLEVEL=9 $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.tif 
 gdal_translate -ot Float32  -co COMPRESS=DEFLATE  -co ZLEVEL=9 $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.tif
 
-gdal_translate -ot Float32  -co COMPRESS=DEFLATE  -co ZLEVEL=9 $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.tif
-gdal_translate -ot Float32  -co COMPRESS=DEFLATE  -co ZLEVEL=9 $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg.tif
+pkfilter -of GTiff -dx 4 -dy 4  -f mean -d 4  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND  -ot Float32  -i  $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.tif
+pkfilter -of GTiff -dx 4 -dy 4  -f mean -d 4  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND  -ot Float32  -i  $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg.nc  $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.tif
 
-gdal_translate -ot Float32  -co COMPRESS=DEFLATE  -co ZLEVEL=9 NETCDF:"$DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.nc":sst    $DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.tif
+gdal_translate -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 NETCDF:"$DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.nc":sst $DIR/reg_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg.tif
 
-echo  observation temporal mean_CRU  mean_HadISST
-
-cdo timmean  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.nc
-cdo timmean  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.nc
-
-cdo timmean  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_1.0deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.nc
-cdo timmean  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_1.0deg.nc   $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.nc
-
-cdo timmean  $DIR/HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc     $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.nc  
-
-observation temporal mean_CRU   transform to tif
+echo observation temporal mean_CRU   transform to tif
 
 gdal_translate -ot Float32  -co COMPRESS=DEFLATE  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.nc  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.tif 
 gdal_translate -ot Float32  -co COMPRESS=DEFLATE  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.nc  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.tif
 
-gdal_translate -ot Float32  -co COMPRESS=DEFLATE  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.nc $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.tif
-gdal_translate -ot Float32  -co COMPRESS=DEFLATE  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.nc $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.tif
+pkfilter -of GTiff -dx 4 -dy 4  -f mean -d 4  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND  -ot Float32  -i  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.nc  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.tif
+pkfilter -of GTiff -dx 4 -dy 4  -f mean -d 4  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND  -ot Float32  -i  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.nc  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.tif
 
 gdal_translate -ot Float32  -co COMPRESS=DEFLATE  NETCDF:"$DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.nc":sst    $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.tif
 
 ' _ 
+
 
 echo create random variable for temperature 0.5 deg mean_CRU
 R --vanilla -q <<EOF
