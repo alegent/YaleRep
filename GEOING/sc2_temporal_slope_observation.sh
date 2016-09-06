41021a7389a726d58a518de966532afd52e000eb
Giuseppe Amatulli of group sbsc <ga254@login-0-0.local>
2016-09-06 15:44:58 -0400

    insert the loop for 2009

--- GEOING/sc2_temporal_slope_observation.sh
+++ GEOING/sc2_temporal_slope_observation.sh
@@ -20,7 +20,7 @@ cdo griddes  $DIR/HadISST/HadISST_sst.nc >  $DIR/HadISST/HadISST_sst_griddes.txt
 
 time frame  1960 - 2005 and  1960 - 2014 
 
-echo 1960.2005  1960.2014 | xargs -n 1 -P 2 bash -c $' 
+echo 1960.2005  1960.2009  1960.2014 | xargs -n 1 -P 3 bash -c $' 
 
 YYYY=$1
 SEQ=$(echo $YYYY  | tr "." " ") 
@@ -55,7 +55,7 @@ echo temperature HadISST
 
 cdo yearmean  -setmissval,-9999 -setvrange,-100,50 -selyear$(for year in $(seq $SEQ); do echo -n ,$year; done) -select,param=-2 $DIR/HadISST/HadISST_sst.nc $DIR/HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.nc  
 
-echo  observation temporal monthly regression 
+echo  observation temporal yearly regression 
 
 cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.pre.dat_0.5deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg.nc
 cdo regres  $DIR/CRU_ts3.23/cru_ts3.23.$YYYY.tmp.dat_0.5deg.nc   $DIR/reg_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg.nc
@@ -114,7 +114,7 @@ EOF
 echo create random variable for precipiation  0.5 deg mean_CRU
 R --vanilla -q <<EOF
 library(raster)
-raster=raster(matrix(runif(259200,max=0.1, min=-0.1),360,720) , xmn=-180, xmx=180, ymn=-90, ymx=190 , crs="+proj=longlat +datum=WGS84 +no_defs")
+raster=raster(matrix(runif(259200,max=0.1, min=0.001),360,720) , xmn=-180, xmx=180, ymn=-90, ymx=190 , crs="+proj=longlat +datum=WGS84 +no_defs")
 writeRaster(raster,filename="/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/mean_CRU/cru_ts3.23.pre.dat_mean_random.0.5deg.tif",options=c("COMPRESS=DEFLATE "),formats=GTiff,overwrite=TRUE)
 EOF
 
@@ -122,7 +122,7 @@ echo create random variable for precipiatation 1 deg mean_CRU
 
 R --vanilla -q <<EOF
 library(raster)
-raster=raster(matrix(runif(64800,max=0.1, min=-0.1),180,360) , xmn=-180, xmx=180, ymn=-90, ymx=190 , crs="+proj=longlat +datum=WGS84 +no_defs")
+raster=raster(matrix(runif(64800,max=0.1, min=0.001),180,360) , xmn=-180, xmx=180, ymn=-90, ymx=190 , crs="+proj=longlat +datum=WGS84 +no_defs")
 writeRaster(raster,filename="/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/mean_CRU/cru_ts3.23.pre.dat_mean_random.1.0deg.tif",options=c("COMPRESS=DEFLATE "),formats=GTiff,overwrite=TRUE)
 EOF
 
@@ -136,7 +136,7 @@ EOF
 
 
 
-echo  1960.2005  1960.2014 | xargs -n 1 -P 2 bash -c $' 
+echo  1960.2005  1960.2009  1960.2014 | xargs -n 1 -P 3 bash -c $' 
 
 YYYY=$1
 SEQ=$(echo $YYYY  | tr "." " ") 
@@ -154,15 +154,16 @@ gdal_calc.py --type=Float32  --NoDataValue=-9999  --outfile=$DIR/mean_HadISST/Ha
 
 # slope in percentage so * 10 to get in km 
 
-gdaldem slope -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope.tif 
-gdaldem slope -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.slope.tif 
+gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope.tif 
+gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.slope.tif 
 
-gdaldem slope -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope.tif 
-gdaldem slope -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope.tif 
+gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope.tif 
+gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope.tif 
 
-gdaldem slope -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope.tif 
+gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope.tif 
 
 # multiply to 10   
+
 gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope10.tif -A $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope.tif --calc="( A.astype(float) * 10 )" --overwrite --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.slope10.tif -A $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.slope.tif --calc="( A.astype(float) * 10 )" --overwrite --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 
@@ -173,7 +174,7 @@ gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_HadISST/Had
 
 # the slope is not = to 0 in any place, due to + random 
 
-# single cell have slope value 0 so mask out
+# single cell have slope value 0 so mask out, masking out islands that are only one grid cell
 
 pksetmask -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope10.tif  -msknodata 0 -p "=" -nodata -9999 -i $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope10.tif -o $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope10msk.tif 
 gdal_edit.py  -a_nodata -9999   $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope10msk.tif
@@ -191,7 +192,6 @@ gdal_edit.py  -a_nodata -9999   $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.s
 pksetmask -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope10.tif -msknodata 0 -p "=" -nodata -9999 -i $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope10.tif -o $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope10msk.tif 
 gdal_edit.py -a_nodata -9999  $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope10msk.tif 
 
-
 echo velocity temporal regression divided  spatial slope  CRU data 
 
 gdal_calc.py --type=Float32  --NoDataValue=-9999 --outfile=$DIR/velocity_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.velocity.tif -A $DIR/reg_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg.tif -B $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope10msk.tif  --calc="( A.astype(float) / ( B.astype(float) ))" --overwrite  --co=COMPRESS=DEFLATE --co=ZLEVEL=9
@@ -206,13 +206,13 @@ gdal_calc.py --type=Float32  --NoDataValue=-9999 --outfile=$DIR/velocity_HadISST
 
 echo  calculate aspect so direction  based on the mean/sum annual temperature/precipitation 
 
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.slope.tif 
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.slope.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.mean.r.tif      $DIR/aspect_CRU/cru_ts3.23.$YYYY.tmp.dat_0.5deg.aspect.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.mean.r.tif      $DIR/aspect_CRU/cru_ts3.23.$YYYY.pre.dat_0.5deg.aspect.tif 
 
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope.tif 
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif      $DIR/slope_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/aspect_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif      $DIR/aspect_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect.tif 
 
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/slope_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/aspect_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect.tif 
 
 
 ' _ 
