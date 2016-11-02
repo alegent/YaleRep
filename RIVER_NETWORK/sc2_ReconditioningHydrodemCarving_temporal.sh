bfd1d0f99567e5304c7d512d93a842c50a7c2b07
Giuseppe Amatulli of group sbsc <ga254@login-0-0.local>
2016-11-02 08:33:35 -0400

    river network delineation

---
+++ RIVER_NETWORK/sc2_ReconditioningHydrodemCarving_temporal.sh
@@ -0,0 +1,34 @@
+# qsub /home/fas/sbsc/ga254/scripts/RIVER_NETWORK/sc2_ReconditioningHydrodemCarving_temporal.sh
+
+#PBS -S /bin/bash 
+#PBS -q fas_normal
+#PBS -l walltime=4:00:00 
+#PBS -l nodes=1:ppn=8
+#PBS -V
+#PBS -o /scratch/fas/sbsc/ga254/stdout
+#PBS -e /scratch/fas/sbsc/ga254/stderr
+
+
+cd     /lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/grassdb
+rm -r   /lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/grassdb/loc_river_tmp
+
+
+
+source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location_grass7.0.2.sh  /lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/grassdb loc_river_tmp /lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/dem/be75_grd_LandEnlarge.tif 
+
+gdal_edit.py  -a_nodata  0  /lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/GSHHG/GSHHS_land_mask250m_enlarge.tif
+gdal_edit.py  -a_nodata -1  /lustre/scratch/client/fas/sbsc/ga254/dataproces/GIW_LANDSAT/tif_250m_from_30m4326/GIW_water_250m_bordermsk_ct.tif
+
+r.in.gdal in=/lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/GSHHG/GSHHS_land_mask250m_enlarge.tif                 out=land_mask250m_enlarge     memory=2000  --overwrite
+r.in.gdal in=/lustre/scratch/client/fas/sbsc/ga254/dataproces/GIW_LANDSAT/tif_250m_from_30m4326/GIW_water_250m_bordermsk_ct.tif   out=GIW_water_250m_bordermsk  memory=2000  --overwrite
+
+echo start r.hydridem   
+#  0 is set as nodata in the geotif so will be report as null 
+r.mask raster=land_mask250m_enlarge
+
+echo start r.mapcalc
+
+r.mapcalc "be75_grd_LandEnlarge_cond_carv  =  be75_grd_LandEnlarge  - ( GIW_water_250m_bordermsk  * 22 )"  --overwrite
+
+r.in.gdal in=/lustre/scratch/client/fas/sbsc/ga254/dataproces/RIVER_NETWORK/GSHHG/GSHHS_land_mask250m_enlarge_clump_UNIT3.tif    out=compUNIT3 memory=2000  --overwrite
+
