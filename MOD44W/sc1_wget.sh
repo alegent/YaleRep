05d951677d02617ada314b8f2b14ddeb96ce8d1e
selvaje <ga254@login-0-0.local>
2017-06-26 04:26:10 -0400

    last push from omega

--- MOD44W/sc1_wget.sh
+++ MOD44W/sc1_wget.sh
@@ -1,4 +1,4 @@
-cd /lustre0/scratch/ga254/dem_bj/MOD44W/gz
+cd  /lustre/scratch/client/fas/sbsc/ga254/dataproces/MOD44W/gz
 
 
 wget -m -A .gz   ftp://ftp.glcf.umd.edu/modis/WaterMask/Collection_5/2000/
@@ -8,7 +8,7 @@ rm -r ftp.glcf.umd.edu
 ls *.gz | xargs -n 1 -P 40 bash -c $' 
 gunzip $1  
 filename=`basename $1 .gz`
-mv /lustre0/scratch/ga254/dem_bj/MOD44W/gz/$filename /lustre0/scratch/ga254/dem_bj/MOD44W/tiles 
+mv   /lustre/scratch/client/fas/sbsc/ga254/dataproces/MOD44W/gz/$filename  /lustre/scratch/client/fas/sbsc/ga254/dataproces/MOD44W/tiles 
 
 ' _
 
