ea760107b4b48681a27c90cff834a7b2d329a58e
selvaje <giuseppe.amatulli@gmail.com>
2017-11-10 16:58:53 -0500

    Update sc2_temporal_slope_observation_after_review.sh

--- GEOING/sc2_temporal_slope_observation_after_review.sh
+++ GEOING/sc2_temporal_slope_observation_after_review.sh
@@ -332,7 +332,7 @@ EOF
 ' _ 
 
 
-# merge statistical the results  
+# make a txt file for the results 
 
 for stat in median mean ; do 
 
@@ -347,10 +347,11 @@ done
 exit
 
 
+####################################################################################
+# calculate slope aspect from spatial gradient to get direction of climate velocity 
+####################################################################################
 
-# echo  calculate aspect so direction  based on the mean/sum annual temperature/precipitation 
-
-## aspect CRU tmp 1.0 
+## aspect CRU tmp 1 degree resolution 
 gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/aspect_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect.tif 
 
 cd  $DIR/direction_CRU
@@ -359,9 +360,9 @@ source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh    
 r.in.gdal in=$DIR/velocity_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.velocity.tif   out=cru_ts3.23.$YYYY.tmp.dat_1.0deg.velocity   --overwrite
 
 r.mask raster=cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect 
-# create an invers direction 
+# create an inverse direction 
 r.mapcalc "cru_ts3.23.$YYYY.tmp.dat_1.0deg.directionNEG = if( cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect  <  180  ,  cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect + 180    ,   cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect - 180  ) " 
-# create a direction map if velocity > 0 , put aspect , esle put the invert direction
+# create a direction map if velocity > 0 , put aspect , else put the inverse direction
 r.mapcalc "cru_ts3.23.$YYYY.tmp.dat_1.0deg.direction = if( cru_ts3.23.$YYYY.tmp.dat_1.0deg.velocity > 0, cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect, cru_ts3.23.$YYYY.tmp.dat_1.0deg.directionNEG) " 
 r.mask -r  # remove the mask 
 # set null to 0 
@@ -372,7 +373,7 @@ r.out.gdal -c     createopt="COMPRESS=DEFLATE,ZLEVEL=9" format=GTiff  type=Float
 rm -fr loc_$YYYY
 gdal_translate  -of netCDF $DIR/direction_CRU/cru_ts3.23.$YYYY.tmp.dat_1.0deg.direction.tif   $DIR/direction_CRU_nc/cru_ts3.23.$YYYY.tmp.dat_1.0deg.direction.nc 
 
-## aspect CRU pre  1.0 
+## aspect CRU precip  1 degree resolution 
 
 gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif      $DIR/aspect_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect.tif 
 
@@ -382,9 +383,9 @@ source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh    
 r.in.gdal in=$DIR/velocity_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.velocity.tif   out=cru_ts3.23.$YYYY.pre.dat_1.0deg.velocity   --overwrite
 
 r.mask raster=cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect 
-# create an invers direction 
+# create an inverse direction 
 r.mapcalc "cru_ts3.23.$YYYY.pre.dat_1.0deg.directionNEG = if( cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect  <  180  ,  cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect + 180    ,   cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect - 180  ) " 
-# create a direction map if velocity > 0 , put aspect , esle put the invert direction
+# create a direction map if velocity > 0 , put aspect , else put the inverse direction
 r.mapcalc "cru_ts3.23.$YYYY.pre.dat_1.0deg.direction    = if( cru_ts3.23.$YYYY.pre.dat_1.0deg.velocity >   0 ,  cru_ts3.23.$YYYY.pre.dat_1.0deg.aspect    ,   cru_ts3.23.$YYYY.pre.dat_1.0deg.directionNEG    ) " 
 r.mask -r  # remove the mask 
 # set null to 0 
@@ -395,9 +396,10 @@ r.out.gdal -c     createopt="COMPRESS=DEFLATE,ZLEVEL=9" format=GTiff  type=Float
 rm -fr loc_$YYYY
 gdal_translate  -of netCDF $DIR/direction_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.direction.tif   $DIR/direction_CRU_nc/cru_ts3.23.$YYYY.pre.dat_1.0deg.direction.nc 
 
+### duplicate 
 
 ## aspect CRU tmp 1.0
-gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif      $DIR/aspect_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect.tif 
+gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif  $DIR/aspect_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.aspect.tif 
 
 cd  $DIR/direction_CRU
 rm -fr loc_$YYYY
@@ -442,7 +444,9 @@ rm -fr loc_$YYYY
 gdal_translate  -of netCDF $DIR/direction_CRU/cru_ts3.23.$YYYY.pre.dat_1.0deg.direction.tif   $DIR/direction_CRU_nc/cru_ts3.23.$YYYY.pre.dat_1.0deg.direction.nc 
 
 
-echo  aspect HadISST  tmp  1.0  aspect HadISST  tmp  1.0  aspect HadISST  tmp  1.0   aspect HadISST  tmp  1.0   aspect HadISST  tmp  1.0  aspect HadISST  tmp  1.0  aspect HadISST  tmp  1.0 aspect HadISST  tmp  1.0 aspect HadISST  tmp  1.0 
+### end duplicate 
+
+# calculate slope aspect HadISST  tmp  1 degree resolution
 
 gdaldem aspect -zero_for_flat -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9  $DIR/mean_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/aspect_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect.tif 
 
@@ -452,15 +456,15 @@ source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh    
 r.in.gdal in=$DIR/velocity_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0deg.velocity.tif    out=HadISST_sst.$YYYY.tmp.dat_1.0deg.velocity   --overwrite
 
 r.mask raster=HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect
-# create an invers direction 
+# create an inverse direction 
 r.mapcalc "  HadISST_sst.$YYYY.tmp.dat_1.0deg.directionNEG  = if( HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect   <  180  ,  HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect  + 180    ,  HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect  - 180  ) " 
 
-echo  create a direction map if velocity > 0 , put aspect , esle put the invert direction
+# create a direction map if velocity > 0 , put aspect , esle put the invert direction
 
 r.mapcalc "HadISST_sst.$YYYY.tmp.dat_1.0deg.direction    = if( HadISST_sst.$YYYY.tmp.dat_1.0deg.velocity >   0 ,  HadISST_sst.$YYYY.tmp.dat_1.0deg.aspect    ,  HadISST_sst.$YYYY.tmp.dat_1.0deg.directionNEG    ) " 
 r.mask -r  # remove the mask 
 
-# set null to 0 
+# set null value to 0 
 r.mapcalc " HadISST_sst.$YYYY.tmp.dat_1.0deg.direction0  = if(  isnull(HadISST_sst.$YYYY.tmp.dat_1.0deg.direction    ) , 0 ,  HadISST_sst.$YYYY.tmp.dat_1.0deg.direction     ) " 
 
 echo export direction 
@@ -472,46 +476,3 @@ gdal_translate -of netCDF  $DIR/direction_HadISST/HadISST_sst.$YYYY.tmp.dat_1.0d
 
 
 exit 
-
-
-# mask out the sea final velocity  =  velocity*_msk.tif 
-
-pksetmask -co COMPRESS=DEFLATE -co ZLEVEL=9 -m  $DIR/mean_CRU10/cru_ts3.23.1901.2014.tmp.dat_velocity_1960-2009.tif -msknodata 100   -p ">"   -nodata -9999  -m   $DIR/mean_CRU10/cru_ts3.23.1901.2014.tmp.dat_slope10_1960-2009.tif   -msknodata 0 -p "=" -nodata -9999  -i $DIR/mean_CRU10/cru_ts3.23.1901.2014.tmp.dat_velocity_1960-2009.tif -o $DIR/mean_CRU10/cru_ts3.23.1901.2014.tmp.dat_velocity_1960-2009_msk.tif
-gdal_edit.py  -a_nodata -9999  $DIR/mean_CRU10/cru_ts3.23.1901.2014.tmp.dat_velocity_1960-2009_msk.tif
-
-pksetmask   -co COMPRESS=DEFLATE  -co ZLEVEL=9  -m  $DIR/mean_CRU10/cru_ts3.23.1901.2014.pre.dat_velocity_1960-2009.tif  -msknodata 100   -p ">"   -nodata -9999  -m   $DIR/mean_CRU10/cru_ts3.23.1901.2014.pre.dat_slope10_1960-2009.tif   -msknodata 0 -p "=" -nodata -9999  -i $DIR/mean_CRU10/cru_ts3.23.1901.2014.pre.dat_velocity_1960-2009.tif -o $DIR/mean_CRU10/cru_ts3.23.1901.2014.pre.dat_velocity_1960-2009_msk.tif
-gdal_edit.py  -a_nodata -9999  $DIR/mean_CRU10/cru_ts3.23.1901.2014.pre.dat_velocity_1960-2009_msk.tif
-
-
-
-# calculate velocity direction 
-
-for PAR in tmp pre ; do 
-
-source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh $DIR/mean_CRU10 loc_${PAR}_velo $DIR/mean_CRU10/cru_ts3.23.1901.2014.${PAR}.dat_velocity_1960-2009_msk.tif 
-
-# r.terraflow -s elevation=cru_ts3.23.1901.2014.${PAR}.dat_velocity_1960-2009_msk direction=cru_ts3.23.1901.2014.${PAR}.dat_direction_1960-2009_msk filled=cru_ts3.23.1901.2014.${PAR}.dat_filled_1960-2009_msk  swatershed=cru_ts3.23.1901.2014.${PAR}.dat_swater_1960-2009_msk accumulation=cru_ts3.23.1901.2014.${PAR}.dat_accum_1960-2009_msk  tci=cru_ts3.23.1901.2014.${PAR}.dat_tci_1960-2009_msk
-
-r.watershed  -s elevation=cru_ts3.23.1901.2014.${PAR}.dat_velocity_1960-2009_msk drainage=cru_ts3.23.1901.2014.${PAR}.dat_direction_1960-2009_msk --overwrite 
-
-for file in $(g.mlist -r  type=rast   pattern="cru_ts3.23.1901.2014.${PAR}.dat_*" --q | grep -v velocity   )  ; do 
-r.out.gdal -c  createopt="COMPRESS=DEFLATE ,ZLEVEL=9" format=GTiff  type=Int32  input=$file    output=$DIR/mean_CRU10/$file.tif  nodata=-9999
-done 
-
-done 
-
-#  
-
-source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh $DIR/mean_HadISST  loc_HadISST_velo $DIR/mean_HadISST10/HadISST_sst.tmp.dat_velocity_1960-2009_msk.tif
-
-r.watershed  -s elevation=HadISST_sst.tmp.dat_velocity_1960-2009_msk   drainage=HadISST_sst.tmp.dat_direction_1960-2009_msk --overwrite 
-
-r.out.gdal -c  createopt="COMPRESS=DEFLATE ,ZLEVEL=9" format=GTiff type=Int32 input=HadISST_sst.tmp.dat_direction_1960-2009_msk   output=$DIR/mean_HadISST10/HadISST_sst.tmp.dat_direction_1960-2009_msk.tif nodata=-9999
-
-rm -r  $DIR/mean_*/loc_* 
-
-
-
-
-
-
