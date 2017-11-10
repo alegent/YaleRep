9e44d45077bd13034f7d7b42f8c79593c0b0e298
selvaje <giuseppe.amatulli@gmail.com>
2017-11-10 16:46:57 -0500

    Update sc2_temporal_slope_observation_after_review.sh
    edit for final manuscript 1st edit

--- GEOING/sc2_temporal_slope_observation_after_review.sh
+++ GEOING/sc2_temporal_slope_observation_after_review.sh
@@ -48,7 +48,7 @@ YSTART=$1
 YEND=$(expr $YSTART + 9)
 RAM=/dev/shm
 ###################################################
-### Calculate the temporal slope using 0.5 degree resolution data. This is what you have done
+### Calculate the temporal slope for every 10-year period (i.e., temporal moving window) using 0.5 degree resolution data.
 ##################################################
 
 # year mean for temperature CRU  ; then regression 
@@ -67,36 +67,35 @@ cdo regres $RAM/cru_ts3.23.$YYYY.pre${YSTART}.${YEND}.dat_0.5deg.nc   $DIR/reg_C
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg${YSTART}.${YEND}.nc -msknodata 100000 -p ">" -nodata -9999 -i $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg${YSTART}.${YEND}.nc -o  $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg${YSTART}.${YEND}.tif
 rm $RAM/cru_ts3.23.$YYYY.pre${YSTART}.${YEND}.dat_0.5deg.nc
 
-# year mean for precipitation  HadISST_sst  ; then regression 
+# year mean for temperature  HadISST_sst  ; then regression 
 
 cdo yearmonmean   -setvals,-1000,-1.8     -setvrange,-100,100 -selyear$(for year in $(seq $YSTART $YEND); do echo -n ,$year; done) -select,param=-2 $DIR/HadISST/HadISST_sst.nc $RAM/HadISST_sst.$YYYY.tmp${YSTART}.${YEND}.dat_1.0deg.nc  
 cdo regres $RAM/HadISST_sst.$YYYY.tmp${YSTART}.${YEND}.dat_1.0deg.nc  $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.nc  
 
-# insert also the mask for label to -9999 the firs row 
+# set the first (i.e., top) row of the HadISST data to no data using -9999 (we did this because some pixels in this row have missing data in some years)
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m /dev/shm/1rowmask.tif -msknodata 0 -p "=" -nodata -9999   -m $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.nc   -msknodata -100000 -p "<" -nodata -9999 -i $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.nc -o $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.tif
 rm -f $RAM/HadISST_sst.$YYYY.tmp${YSTART}.${YEND}.dat_1.0deg.nc
 
 ##################################################################
-### Regrid the temporal slope data for each 10 yr period to 1 degree by averaging across 2x2 neighborhoods.
+### Regrid the temporal slope for CRU data for each 10 yr period to 1 degree by averaging across 2x2 neighborhoods.
 #################################################################
 
 pkfilter -of GTiff -dx 2 -dy 2  -f mean -d 2  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND -nodata -9999 -ot Float32  -i $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_0.5deg.reg${YSTART}.${YEND}.tif  -o $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg${YSTART}.${YEND}.tif
-
 pkfilter -of GTiff -dx 2 -dy 2  -f mean -d 2  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND -nodata -9999 -ot Float32  -i $DIR/reg_CRU10/cru_ts3.23.$YYYY.tmp.dat_0.5deg.reg${YSTART}.${YEND}.tif  -o $DIR/reg_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.tif
 
 ' _ 
 
+##################################################################
+#### calculate the area-weighted median and area-weighted mean (with real values and absolute values) 
+#### for the temporal slopes
+##################################################################
 
+#### First step: create a ratser with the area of each pixel, and then mask these area rasters with the nodata pixels from CRU and HadISST dataset 
 
-#### calculate the weighted median and weighted mean (with real values and absolute values) 
-
-#### run it using one 10-years period
-#### mask the AERA raster with cru and HadISST dataset 
-
-pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/reg_CRU10/cru_ts3.23.1960.2014.pre.dat_1.0deg.reg1960.1969.tif -msknodata -9999  -nodata -9999 -i /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEO_AREA/area_tif/1.00deg-Area_prj6965.tif -o /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_CRU.tif
-
+pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9  -m $DIR/reg_CRU10/cru_ts3.23.1960.2014.pre.dat_1.0deg.reg1960.1969.tif -msknodata -9999  -nodata -9999 -i /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEO_AREA/area_tif/1.00deg-Area_prj6965.tif -o /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_CRU.tif
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9  -m /dev/shm/1rowmask.tif -msknodata 0 -p "="  -nodata -9999  -m $DIR/reg_HadISST10/HadISST_sst.1960.2014.tmp.dat_1.0deg.reg1960.1969.tif   -msknodata -9999 -p "="  -nodata -9999 -i /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEO_AREA/area_tif/1.00deg-Area_prj6965.tif -o /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_HadISST.tif
 
+#### Second step: specify 10-year moving window
 seq 1960 2005 | xargs -n 1 -P 8 bash -c $' 
 
 export YYYY=1960.2014
@@ -124,10 +123,10 @@ for (var  in c("tmp" , "pre")){
 value=na.omit(as.vector(raster(paste0(DIR ,"/reg_CRU10/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.reg",YSTART,".",YEND,".tif"))),mode = "numeric")
 weight=na.omit(as.vector(raster("/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_CRU.tif")) , mode = "numeric")
 
-median=bigvis::weighted.median(abs(value),weight)
+median=bigvis::weighted.median(value,weight)
 write.table(median, paste0(DIR,"/reg_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.regAREA",YSTART,".",YEND,"weightedmedian.txt"), col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
-mean=stats::weighted.mean(abs(value),weight)
+mean=stats::weighted.mean(value,weight)
 write.table(mean, paste0(DIR,"/reg_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.regAREA",YSTART,".",YEND,"weightedmean.txt"), col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
 
@@ -136,10 +135,10 @@ write.table(mean, paste0(DIR,"/reg_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat_1.0d
 value=na.omit(as.vector(raster( paste0(DIR ,"/reg_HadISST10/HadISST_sst." , YYYY , ".tmp.dat_1.0deg.reg" , YSTART ,"." , YEND ,".tif" ))) , mode = "numeric")
 weight=na.omit(as.vector(raster("/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_HadISST.tif")) , mode = "numeric")
 
-median=bigvis::weighted.median(abs(value),weight )
+median=bigvis::weighted.median(value,weight )
 write.table(median, paste0(DIR,"/reg_HadISST10txt/HadISST_sst.",YYYY,".tmp.dat_1.0deg.regAREA",YSTART,".",YEND,"weightedmedian.txt" ), sep = " " , col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
-mean=stats::weighted.mean(abs(value),weight )
+mean=stats::weighted.mean(value,weight )
 write.table(mean, paste0(DIR,"/reg_HadISST10txt/HadISST_sst.",YYYY,".tmp.dat_1.0deg.regAREA",YSTART,".",YEND,"weightedmean.txt" ), sep = " " , col.names = FALSE , quote = FALSE , row.names=FALSE  )
 }
 
@@ -147,7 +146,7 @@ EOF
 
 ' _ 
 
-# merge statistical the results 
+# merge results into txt files
 
 for stat in median mean ; do 
 
@@ -157,22 +156,25 @@ cat  $DIR/reg_CRU10txt/cru_ts3.23.1960.2014.pre.dat_1.0deg.regAREA????.????weigh
 cat $DIR/reg_HadISST10txt/HadISST_sst.1960.2014.tmp.dat_1.0deg.regAREA????.????weighted$stat.txt | sort -g > $DIR/reg_HadISST10txt/HadISST_sst.1960.2014.tmp.dat_1.0deg.regAREA_allyear_weighted$stat.txt
 
 done 
+##################################################
+## calculate spatial gradients in temp and precipitation 
+###############################################
 
-# long term mean to calculate spatial slope 
+##first step: calculate long-term mean 1960-2014
 
 YYYY=1960.2014
 
 cdo yearmonmean  -setmissval,-9999 -setvrange,-100,100 -selyear$(for year in $(seq 1960 2014 ) ; do echo -n ,$year ; done) $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.tmp.dat.nc   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.nc
-
 cdo yearsum  -setmissval,-9999 -setvrange,0,4000 -selyear$(for year in $(seq 1960 2014 ) ; do echo -n ,$year ; done) $DIR/CRU_ts3.23/cru_ts3.23.1901.2014.pre.dat.nc    $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.nc
 
-# year mean 
+# calculate 1960-2014 mean 
 cdo timmean  $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.nc   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.mean.nc
 cdo timmean  $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.nc   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.mean.nc
 
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.mean.nc    -msknodata 100000 -p ">" -nodata -9999 -i   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.mean.nc -o   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.mean.tif
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.mean.nc    -msknodata 100000 -p ">" -nodata -9999 -i   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.mean.nc -o   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.mean.tif
 
+#aggregate CRU mean to 1 degree using 2x2 neighbourhoods
 pkfilter -of GTiff -dx 2 -dy 2  -f mean -d 2  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND -nodata -9999 -ot Float32  -i   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_0.5deg.mean.tif  -o   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.tmp.dat_1.0deg.mean.tif
 pkfilter -of GTiff -dx 2 -dy 2  -f mean -d 2  -co COMPRESS=DEFLATE -co ZLEVEL=9 -co INTERLEAVE=BAND -nodata -9999 -ot Float32  -i  $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_0.5deg.mean.tif  -o   $DIR/mean_CRU10/cru_ts3.23.${YYYY}.pre.dat_1.0deg.mean.tif
 
@@ -184,8 +186,8 @@ cdo timmean  $DIR/mean_HadISST10/HadISST_sst.${YYYY}.tmp.dat_1.0deg.nc  $DIR/mea
 
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/mean_HadISST10/HadISST_sst.${YYYY}.tmp.dat_1.0deg.mean.nc   -msknodata -100000 -p "<" -nodata -9999 -i $DIR/mean_HadISST10/HadISST_sst.${YYYY}.tmp.dat_1.0deg.mean.nc  -o $DIR/mean_HadISST10/HadISST_sst.${YYYY}.tmp.dat_1.0deg.mean.tif
 
-# echo create random variable for temperature 1 deg mean_CRU
-
+# add uniformly distributed random noise 
+# for temperature at 1 deg on long-term mean of CRU
 R --vanilla -q <<EOF
 library(raster , lib.loc = "/usr/local/cluster/hpc/Rpkgs/RASTER/2.5.2/3.0/" ) 
 library(rgdal , lib.loc = "/usr/local/cluster/hpc/Rpkgs/RGDAL/0.9-3/3.0" )
@@ -193,7 +195,7 @@ raster=raster(matrix(runif(64800,max=0.05, min=-0.05),180,360) , xmn=-180, xmx=1
 writeRaster(raster,filename="/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/mean_CRU10/cru_ts3.23.tmp.dat_mean_random.1.0deg.tif",options=c("COMPRESS=DEFLATE "),formats=GTiff,overwrite=TRUE)
 EOF
 
-echo create random variable for precipiatation 1 deg mean_CRU
+# create random variable for precipiatation 1 deg long-term mean CRU
 
 R --vanilla -q <<EOF
 library(raster , lib.loc = "/usr/local/cluster/hpc/Rpkgs/RASTER/2.5.2/3.0/" ) 
@@ -202,7 +204,7 @@ raster=raster(matrix(runif(64800,max=0.1, min=0.001),180,360) , xmn=-180, xmx=18
 writeRaster(raster,filename="/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/mean_CRU10/cru_ts3.23.pre.dat_mean_random.1.0deg.tif",options=c("COMPRESS=DEFLATE "),formats=GTiff,overwrite=TRUE)
 EOF
 
-echo create random variable for temperature 1 deg mean_HadISST
+#create random variable for temperature 1 deg long-term mean_HadISST
 
 R --vanilla -q <<EOF
 library(raster , lib.loc = "/usr/local/cluster/hpc/Rpkgs/RASTER/2.5.2/3.0/" ) 
@@ -211,25 +213,25 @@ raster=raster(matrix(runif(64800,max=0.05, min=-0.05),180,360) , xmn=-180, xmx=1
 writeRaster(raster,filename="/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/mean_HadISST10/HadISST_sst.tmp.dat_mean_random.1.0deg.tif",options=c("COMPRESS=DEFLATE "),formats=GTiff,overwrite=TRUE)
 EOF
 
-echo  1960.200
+echo  1960.2000
 YYYY=1960.2014
 SEQ=$(echo $YYYY  | tr "." " ") 
 
-echo add the random to temporal mean for precipitation and temperature 
+# add the random noise to temporal mean for precipitation and temperature 
 
 gdal_calc.py --type=Float32  --NoDataValue=-9999  --outfile=$DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif -A $DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.tif -B $DIR/mean_CRU10/cru_ts3.23.tmp.dat_mean_random.1.0deg.tif --calc="( A.astype(float) +  B.astype(float) )"  --overwrite    --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 gdal_calc.py --type=Float32  --NoDataValue=-9999  --outfile=$DIR/mean_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif -A $DIR/mean_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.tif -B $DIR/mean_CRU10/cru_ts3.23.pre.dat_mean_random.1.0deg.tif --calc="( A.astype(float) +  B.astype(float) )"  --overwrite  --co=COMPRESS=DEFLATE   --co=ZLEVEL=9
 
 gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/mean_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif -A $DIR/mean_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.tif -B $DIR/mean_HadISST10/HadISST_sst.tmp.dat_mean_random.1.0deg.tif  --calc="( A.astype(float) +  B.astype(float) )" --overwrite   --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 
-# slope in percentage so * 10 to get in km 
+# calculate spatial slope
 
 gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9 $DIR/mean_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.mean.r.tif  $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope.tif 
 gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9 $DIR/mean_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.mean.r.tif  $DIR/slope_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope.tif 
 
 gdaldem slope -p -compute_edges -s 111120 -co COMPRESS=DEFLATE -co ZLEVEL=9 $DIR/mean_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.mean.r.tif $DIR/slope_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope.tif
 
-# multiply to 10   
+# spatial gradient is returned as  percentage so multiply by 10 to get km
 
 gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope10.tif -A $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope.tif --calc="( A.astype(float) * 10 )" --overwrite --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope10.tif -A $DIR/slope_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope.tif --calc="( A.astype(float) * 10 )" --overwrite --co=COMPRESS=DEFLATE --co=ZLEVEL=9
@@ -238,7 +240,7 @@ gdal_calc.py --type=Float32 --NoDataValue=-9999 --outfile=$DIR/slope_HadISST10/H
 
 # the slope is not = to 0 in any place, due to + random 
 
-# single cell have slope value 0 so mask out, masking out islands that are only one grid cell
+# single pixels with no neighbours have slope value 0 so mask out, also masking out islands that are only one grid cell
 
 pksetmask -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope10.tif  -msknodata 0 -p "=" -nodata -9999 -i $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope10.tif -o $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope10msk.tif 
 gdal_edit.py  -a_nodata -9999   $DIR/slope_CRU10/cru_ts3.23.$YYYY.tmp.dat_1.0deg.slope10msk.tif
@@ -257,8 +259,9 @@ pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_CRU10/cru_
 
 pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope10msk.tif -msknodata -9999  -nodata -9999 -i /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEO_AREA/area_tif/1.00deg-Area_prj6965.tif -o /lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_CRUslopepre.tif
 
-echo  start to calculate the velocity for the 10-year period 
-
+##########################################################
+### calculate climate velocity for the 10-year periods 
+###########################################################
 seq 1960 2005 | xargs -n 1 -P 8 bash -c $' 
 
 export YYYY=1960.2014
@@ -266,7 +269,7 @@ export YSTART=$1
 export YEND=$(expr $YSTART + 9)
 export RAM=/dev/shm
 
-echo velocity temporal regression divided spatial slope CRU data 
+# calculate velocity by dividing temporal regression by spatial slope CRU data 
 
 gdal_calc.py --type=Float32  --NoDataValue=-9999 --outfile=$RAM/cru_ts3.23.$YYYY.pre.dat_1.0deg.velocity${YSTART}.${YEND}.tif -A $DIR/reg_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.reg${YSTART}.${YEND}.tif  -B $DIR/slope_CRU10/cru_ts3.23.$YYYY.pre.dat_1.0deg.slope10msk.tif  --calc="( A.astype(float) / ( B.astype(float) ))" --overwrite  --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 
@@ -278,7 +281,7 @@ pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m $DIR/slope_CRU10/cru_
 
 rm $RAM/cru_ts3.23.$YYYY.???.dat_1.0deg.velocity${YSTART}.${YEND}.tif 
 
-echo velocity temporal regression divided spatial slope HadISST data   $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.tif
+# calculate velocity by dividing temporal regression by spatial slope HadISST data   
 
 gdal_calc.py --type=Float32  --NoDataValue=-9999 --outfile=$RAM/HadISST_sst.$YYYY.tmp.dat_1.0deg.velocity${YSTART}.${YEND}.tif  -A $DIR/reg_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.reg${YSTART}.${YEND}.tif  -B  $DIR/slope_HadISST10/HadISST_sst.$YYYY.tmp.dat_1.0deg.slope10msk.tif  --calc="( A.astype(float) / ( B.astype(float) ))" --overwrite  --co=COMPRESS=DEFLATE --co=ZLEVEL=9
 
@@ -286,7 +289,7 @@ pksetmask -ot Float32 -co COMPRESS=DEFLATE -co ZLEVEL=9 -m /dev/shm/1rowmask.tif
 rm   $RAM/HadISST_sst.$YYYY.tmp.dat_1.0deg.velocity${YSTART}.${YEND}.tif
 
 ######################
-#### Calculate area-weighted global mean and median for the velocity  (temporal regression )for each 10 yr period using the 1 degree resolution 
+#### Calculate area-weighted global mean and median for the velocity for each 10 yr period using the 1 degree resolution 
 ######################
 
 R --vanilla -q <<EOF
@@ -306,10 +309,10 @@ for (var  in c("tmp" , "pre")){
 value=na.omit(as.vector(raster(paste0(DIR ,"/velocity_CRU10/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.velocity",YSTART,".",YEND,".tif"))),mode = "numeric")
 weight=na.omit(as.vector(raster(paste0("/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_CRUslope",var,".tif"))) , mode = "numeric")
 
-median=bigvis::weighted.median(abs(value),weight)
+median=bigvis::weighted.median(value,weight)
 write.table(median, paste0(DIR,"/velocity_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.velocityAREA",YSTART,".",YEND,"weightedmedian.txt"), col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
-mean=stats::weighted.mean(abs(value),weight)
+mean=stats::weighted.mean(value,weight)
 write.table(mean, paste0(DIR,"/velocity_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat_1.0deg.velocityAREA",YSTART,".",YEND,"weightedmean.txt"), col.names = FALSE , quote = FALSE , row.names=FALSE  )
 }
 
@@ -318,10 +321,10 @@ write.table(mean, paste0(DIR,"/velocity_CRU10txt/cru_ts3.23.",YYYY,".",var,".dat
 value=na.omit(as.vector(raster( paste0(DIR ,"/velocity_HadISST10/HadISST_sst." , YYYY , ".tmp.dat_1.0deg.velocity" , YSTART ,"." , YEND ,".tif" ))) , mode = "numeric")
 weight=na.omit(as.vector(raster("/lustre/scratch/client/fas/sbsc/ga254/dataproces/GEOING/GEO_AREA/1.00deg-Area_prj6965_HadISSTslope.tif")) , mode = "numeric")
 
-median=bigvis::weighted.median(abs(value),weight )
+median=bigvis::weighted.median(value,weight )
 write.table(median, paste0(DIR,"/velocity_HadISST10txt/HadISST_sst.",YYYY,".tmp.dat_1.0deg.velocityAREA",YSTART,".",YEND,"weightedmedian.txt" ), sep = " " , col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
-mean=stats::weighted.mean(abs(value),weight )
+mean=stats::weighted.mean(value,weight )
 write.table(mean, paste0(DIR,"/velocity_HadISST10txt/HadISST_sst.",YYYY,".tmp.dat_1.0deg.velocityAREA",YSTART,".",YEND,"weightedmean.txt" ), sep = " " , col.names = FALSE , quote = FALSE , row.names=FALSE  )
 
 EOF
